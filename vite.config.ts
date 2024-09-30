974af9619601ccd4e0ebe39927e5d61f2ef8d1d3
Blake Ball <blakesteve@gmail.com>
2024-09-29 23:00:28 -0500

    add env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Check if VITE_API_URL is defined
+const apiUrl = "https://pickems.ball.farm/";
+
+if (!apiUrl) {
+  throw new Error("VITE_API_URL is not defined. Please check your .env files.");
+}
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/games": {
-        target: "https://pickems.ball.farm/",
+        target: apiUrl, // Use the environment variable
         changeOrigin: true,
       },
     },
