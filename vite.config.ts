0c384286e5aaf70dc0a5594882a8fe17e5c30e18
Blake Ball <blakesteve@gmail.com>
2024-09-30 22:26:40 -0500

    trying a different config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import * as process from "process";
-import { config } from "dotenv";
 
-// Load environment variables from .env
-config();
+// Load environment variables
+const env = loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
-console.log("VITE_API_URL:", process.env.VITE_API_URL); // Check if VITE_API_URL is defined
+console.log("VITE_API_URL:", env.VITE_API_URL); // Check if VITE_API_URL is defined
 
-const apiUrl = process.env.VITE_API_URL;
+const apiUrl = env.VITE_API_URL;
 
 if (!apiUrl) {
   throw new Error("VITE_API_URL is not defined. Please check your .env files.");
