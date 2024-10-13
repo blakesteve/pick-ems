49c8e78c9b20a41a7631acf569053636243db0df
Blake Ball <blakesteve@gmail.com>
2024-10-13 15:45:30 -0500

    dynamically calculate the current week

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { differenceInCalendarDays } from "date-fns";
 import Games from "./Games";
 // import Box from "@mui/material/Box";
 import { Box, CircularProgress } from "@mui/material";
@@ -18,21 +19,42 @@ interface Game {
 
 function Home() {
   const [games, setGames] = useState<Game[]>([]);
-  // State to handle loading or errors
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  // needs to be updated every new season
+  const getCurrentWeek = () => {
+    // first relevant day of the season (Thursday, September 5th, 2024)
+    const firstDay = new Date(2024, 8, 5);
+    const currentDate = new Date();
+
+    // Calculate the number of days difference between the current date and the first day
+    let dayDifference = differenceInCalendarDays(currentDate, firstDay);
+
+    // Since weeks start on Tuesday, we need to offset the day difference by 2 days (Thursday -> Tuesday)
+    dayDifference -= 2;
+
+    // If the offset leads to a negative number of days, week 1 is still the current week
+    if (dayDifference < 0) {
+      return 1;
+    }
+
+    // Calculate the current week number
+    const currentWeek = Math.floor(dayDifference / 7) + 1;
+
+    return currentWeek;
+  };
+
   useEffect(() => {
-    // Define the function to fetch data
+    const currentWeek = getCurrentWeek();
     const fetchGames = async () => {
       try {
-        const response = await fetch("/games/week/4");
+        const response = await fetch(`/games/week/${currentWeek}`);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        const data: Game[] = await response.json(); // Adjust the type as needed
+        const data: Game[] = await response.json();
         setGames(data);
-        console.log(data); // Log the result to the console
       } catch (error: any) {
         setError(error.message);
       } finally {
